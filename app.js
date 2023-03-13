709ae26fbc8fba4641726909f25e4043be4df076
stormymayday <stormymayday7@gmail.com>
2023-03-12 20:55:19 -0600

    Displaying the date dynamically.

--- app.js
+++ app.js
@@ -28,5 +28,8 @@ const deadline = document.querySelector('.deadline');
 const items = document.querySelectorAll('.deadline-format h4');
 
 let futureDate = new Date(2020, 4, 24, 11, 30, 0);
+// Extracting the year
 const year = futureDate.getFullYear();
 console.log(year);
+// Assigning the date
+giveaway.textContent = `giveaway ends on ${year}`;
